cf3721c48baf11d2bbedeaf629b7ad4653cb1997
Ruchika 16 <ruchik16@in.ibm.com>
2021-07-08 10:16:37 +0530

    ES6 Promises practice

--- JS-Practice Projects/employee.js
+++ JS-Practice Projects/employee.js
@@ -21,3 +21,29 @@ class Employee {
         }
     }
 }
+
+class ContractEmp extends Employee {
+    constructor(firstName, lastName, title, salary, hrsPerWeek) {
+        super(firstName, lastName, title, salary);
+
+        this._hrsPerWeek = hrsPerWeek;
+    }
+    info() {
+        if (this._active) {
+            const info = `${this._firstName} ${this._lastName}, ${this._title} is active
+            with ${this._hrsPerWeek} uti`;
+            return info;
+        } else {
+            const info = "Employee is not active !!";
+            return info;
+        }
+    }
+
+    set hours(newHrs) {
+        if (newHrs < 40) {
+            this._hrsPerWeek = newHrs;
+        } else {
+            alert("Hourse out of range of part-time work!!");
+        }
+    }
+}
