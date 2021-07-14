ca64bb6a738e0d44f2ab5cfd85d7df1a8f45db50
Ruchika 16 <ruchik16@in.ibm.com>
2021-07-14 14:33:09 +0530

    Project structuring

---
+++ MERN_Projects/MERN_Mongodb/mongo.js
@@ -0,0 +1,16 @@
+const MongoClient = require('mongodb').MongoClient;
+
+const url = 'mongodb+srv://ruchi:8If0SAN6JME2hoUy@cluster0.ppihy.mongodb.net/products_test?retryWrites=true&w=majority';
+
+
+const createProduct = async (req, res, next) => {
+
+}
+
+const getProducts = async (req, res, next) => {
+
+}
+
+
+exports.createProduct = createProduct;
+exports.getProducts = getProducts;
