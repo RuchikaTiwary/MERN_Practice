eff2c9df07faeeb65095c51745721e46aafdaa63
Ruchika 16 <ruchik16@in.ibm.com>
2021-07-14 19:08:42 +0530

    Added mongo db project

--- MERN_Projects/MERN_Mongodb/mongo.js
+++ MERN_Projects/MERN_Mongodb/mongo.js
@@ -1,10 +1,26 @@
 const MongoClient = require('mongodb').MongoClient;
 
-const url = 'mongodb+srv://ruchi:8If0SAN6JME2hoUy@cluster0.ppihy.mongodb.net/products_test?retryWrites=true&w=majority';
+const url = 'mongodb+srv://Tanu:v644byTnjNmWFLNq@cluster0.ppihy.mongodb.net/products_test?retryWrites=true&w=majority';
 
-
+// v644byTnjNmWFLNq
 const createProduct = async (req, res, next) => {
+    const newProduct = {
+        name: req.body.name,
+        price: req.body.price
+    };
 
+    const client = new MongoClient(url); //which server we want to connect to
+
+    try{
+        await client.connect();  //establish connection with mongodb server
+        const db = client.db();     //access specific db on server
+        const result = db.collection('products').insertOne(newProduct);
+
+    } catch(error){
+        return res.json({message: 'Could not connect to DB!!'})
+    }
+    // client.close();
+    res.json(newProduct);
 }
 
 const getProducts = async (req, res, next) => {
